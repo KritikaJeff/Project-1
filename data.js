30a0424cc580c4b1567e2c8802ee7835d2ceeaca
KritikaJeff <as.kritika.jeff@gmail.com>
2024-04-10 13:46:16 +0200

    commit

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-keuken",
-      "name": "keuken",
+      "id": "0-360-2",
+      "name": "360 2",
       "levels": [
         {
           "tileSize": 256,
@@ -26,17 +26,17 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.09387522130205994,
-          "pitch": 0,
+          "yaw": -2.9286966802767136,
+          "pitch": 0.13944591421041075,
           "rotation": 0,
-          "target": "4-keuken-2"
+          "target": "1-360-1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-leefruimte-2",
-      "name": "leefruimte 2",
+      "id": "1-360-1",
+      "name": "360 1",
       "levels": [
         {
           "tileSize": 256,
@@ -60,139 +60,10 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0,
-          "pitch": 0,
+          "yaw": 0.6288644055242294,
+          "pitch": 0.037403590008722176,
           "rotation": 0,
-          "target": "2-leefruimte"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-leefruimte",
-      "name": "leefruimte",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.07721707917902876,
-          "pitch": 0.045071824004171646,
-          "rotation": 0,
-          "target": "3-eetruimte"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-eetruimte",
-      "name": "eetruimte",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "4-keuken-2",
-      "name": "keuken 2",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "yaw": 0,
-        "pitch": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.019828349350586194,
-          "pitch": 0.5368957873714866,
-          "rotation": 0,
-          "target": "5-keuken-3"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "5-keuken-3",
-      "name": "keuken 3",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.7571191108003319,
-          "pitch": 0.3335946015399607,
-          "rotation": 0,
-          "target": "0-keuken"
+          "target": "0-360-2"
         }
       ],
       "infoHotspots": []
@@ -200,9 +71,9 @@ var APP_DATA = {
   ],
   "name": "Project Title",
   "settings": {
-    "mouseViewMode": "qtvr",
-    "autorotateEnabled": true,
-    "fullscreenButton": true,
-    "viewControlButtons": true
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
   }
 };
